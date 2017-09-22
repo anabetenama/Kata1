a4f3c8fba861ca89774ed00865e1ef1e1f42118c
usuario <usuario@10.22.148.61>
2017-09-22 13:47:56 +0100

    Kata1: Se calcula la edad de una persona usando la clase obsoleta Date

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,12 +1,16 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Ana ", new Date(97,9,24));
-        System.out.println(person.getName() + "tiene " + 
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(1997, 9, 24);
+        
+        Person person = new Person("Ana", date);
+        System.out.println(person.getName() + " tiene " + 
                 person.getAge() + " años");
     }
 }
